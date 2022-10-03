1a545d4e456068c3e81a87f49446fa66a1d11f4b
MIZ-KAS <kshakirat0@gmail.com>
2022-10-03 09:24:29 +0000

    checking

--- terraform/config.sh
+++ terraform/config.sh
@@ -19,3 +19,4 @@ az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOU
 echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
 echo "container_name: $CONTAINER_NAME"
 echo "access_key: $ACCOUNT_KEY"
+
